843acb3edd50722f2ea09024921b385ad0b93465
Siva Kumar <siva3378@gmail.com>
2018-11-11 22:53:29 +0000

    FEAT: [tasks] - to learn loops in js

--- shopping-site/assignments/006 - loops/loops.js
+++ shopping-site/assignments/006 - loops/loops.js
@@ -8,7 +8,24 @@ const jsonObject = {
 }
 
 // 1. Write a for loop to print list of items in stringArray
+// for (let i = 0; i < stringArray.length; i++) {
+//   console.log("Name : ", stringArray[i])
+// };
+
+stringArray.forEach(function (element) {
+  console.log("Name : ", element);
+})
+console.log("**************************")
+
 
 // 2. write a for loop to print list of names from objectArray
+objectArray.forEach(function (element) {
+  console.log("Name>> Object :", element.name)
+})
+console.log("**************************")
 
 // 3. write a for loop to prnt key & values from jsonObject
+for (let key in jsonObject) {
+  console.log(key + '-' + jsonObject[key])
+}
+console.log("**************************")
